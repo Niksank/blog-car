727bfda7d40e0f25b69b9c2c1043e79be2fb030a
= <=>
2019-02-28 15:16:28 +0100

    package json

---
+++ blog/pages/upload-image-article.php
@@ -0,0 +1,47 @@
+<?php
+  session_start();
+  if(isset($_SESSION['email'])){
+    $id = $_SESSION['id'];
+    include '../bdd/database.php';
+    global $bdd;  
+  }
+  else if(!isset($_SESSION['email'])){
+    session_destroy();
+    header('Location: login.php');
+  }
+$statusMsg = '';
+
+
+// File upload path
+$targetDir = "../uploads/";
+$fileName = basename($_FILES["file"]["name"]);
+$targetFilePath = $targetDir.$_GET['name'].'-'.$fileName;
+$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
+    // Allow certain file formats
+    $allowTypes = array('jpg','png','jpeg','gif','pdf');
+    if(in_array($fileType, $allowTypes)){
+        // Upload file to server
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+            // Insert image file name into database
+            $insert = $bdd->query(" UPDATE `user` SET `image`= '".$_GET['name']."-".$fileName."' WHERE `name`='".$_GET['name']."'");
+            if($insert){
+                $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+                header('Location: ../account.php');
+            }else{
+                $statusMsg = "File upload failed, please try again.";
+            } 
+        }else{
+            $statusMsg = "Sorry, there was an error uploading your file. ". $targetFilePath. "";
+        }
+    }else{
+        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+    }
+}else{
+    $statusMsg = 'Please select a file to upload.';
+}
+
+// Display status message
+echo $statusMsg;
+?>
